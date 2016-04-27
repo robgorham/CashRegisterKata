9cbc06edd9e77d05d70994d635111c3e77403bf3
Robert Gorham <robsteeliano@gmail.com>
2016-04-27 10:33:27 -0400

    Added Benjies

--- CashRegisterKata/Program.cs
+++ CashRegisterKata/Program.cs
@@ -9,7 +9,7 @@ namespace CashRegisterKata
     class Program
     {
 
-        
+
 
         static void Main(string[] args)
         {
@@ -22,24 +22,24 @@ namespace CashRegisterKata
 
         static string ProcessChange(decimal input)
         {
-            string[,] names = { { "Twenty", "Twenties" }, { "Ten", "Tens" }, { "Five", "Fives" }, { "One", "Ones" },
+            string[,] names = { {"Benjie","Benjies" },{"Fifty","Fifties" }, { "Twenty", "Twenties" }, { "Ten", "Tens" }, { "Five", "Fives" }, { "One", "Ones" },
                 { "Quarter", "Quarters" }, { "Dime", "Dimes" }, { "Nickel", "Nickels" }, { "Penny", "Pennies" } };
-            decimal[] denoms = { 20m, 10m, 5m, 1m, .25m, .1m, .05m, .01m };
-            int[] count = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            decimal[] denoms = { 100m, 50m, 20m, 10m, 5m, 1m, .25m, .1m, .05m, .01m };
+            int[] count = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             string result = "";
-            for(int i = 0; i < 8; i++)
+            for (int i = 0; i < count.Length; i++)
             {
-                count[i] =(int) decimal.Floor(input / denoms[i]);
-                input -= 1m * count[i] * denoms[i];
+                count[i] = (int)decimal.Floor(input / denoms[i]);
+                input -= denoms[i] * count[i];
             }
             result += "You Received :";
-            for(int i= 0;i< 8;i++)
+            for (int i = 0; i < count.Length; i++)
             {
-                if(count[i]> 0)
+                if (count[i] > 0)
                 {
-                    if(count[i]==1)
+                    if (count[i] == 1)
                     {
-                        result +=" " + count[i] + " " + names[i, 0];
+                        result += " " + count[i] + " " + names[i, 0];
                         //Console.Write("{0} {1},", count[i], names[i, 0]);
                     }
                     else
